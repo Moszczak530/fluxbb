3c2b06564d5c225f849354ac132376c7a3356fc2
Moszczak530 <65900935+Moszczak530@users.noreply.github.com>
2021-12-07 22:51:44 +0100

    Polish lang

--- lang/Polish/payment.php
+++ lang/Polish/payment.php
@@ -2,24 +2,24 @@
 
 // Language definitions used in payment.php, newpayment.php, payment_query.php and motify_payments.php
 $lang_payment = array(
-    'PayPal 1'			    =>	'',
-    'PayPal 2'              =>  '',
-    'PayPal 3'              =>  '',
-    'PayPal 4'              =>  '',
-    'PayPal 5'              =>  '',
-    'Plan'                  =>  '',
-    'Transaction ID'        =>  '',
-    'Billing Email'         =>  '',
-	'Billing Email Tooltip' =>  '',
-    'Submit'                =>  '',
-    'Cancel'                =>  '',
-	'501'                   =>  '',
-    '403'                   =>  '',
-    '400'                   =>  '',
-	'429'          			=>  '',
-    'Invoice confirmation'  =>  '',
-    'Success'               =>  '',
-    'Success Text'          =>  '',
-    'Failed' 				=>  '',
-    'Awaiting Approval'     =>  '',
-  );
+    'PayPal 1'              =>  'Zaloguj się na swoje konto PayPal używając swojej przeglądarki. Kliknij w przycisk %sWyślij%s na górze strony. W pole "e-mail" wpisz adress email podany poniżej.',
+    'PayPal 2'              =>  'Wpisz %s jako ilość do wysłania, EUR jako walute, oraz wiadomość poniżej jako uwagę a następnie kliknij %sKontynuuj%s.',
+    'PayPal 3'              =>  'Następnie wybierz opcje poniżej jako %sWysyłam pieniądze do znajomego%s, gdy się pojawi.',
+    'PayPal 4'              =>  'Musisz wysłać pieniądzę używając %sWysyłam pieniądze do znajomego%s! Wszystkie inne transakcje niż za pomocą &apos;Znajomij i Przyjaciele&apos; będą skutkowały permamentnym zbanowaniem twojego konta!',
+    'PayPal 5'              =>  'Dokończ płatność. Upewnij się że numer transakcji oraz twój adres emial są powiązane z twoim kontem PayPal poniżej. Administrator sprawdzi twoją płatność manualnie. Może to potrwać do 24 godzin więc prosimy, bądź cierpliwy!',
+    'Plan'                  =>  '%s za %s dni',
+    'Transaction ID'        =>  'Numer transakcji',
+    'Billing Email'         =>  'Email rozliczeniowy',
+    'Billing Email Tooltip' =>  'Wpisz swój email powiązany z twoim kontem PayPal dla możliwych zwrotów albo dla weryfikacji',
+    'Submit'                =>  'Zatwierdź',
+    'Cancel'                =>  'Anuluj',
+    '501'                   =>  'Niestety ta metoda płatności jest obecnie niedostępna. Spróbuj w inny sposób.', // Payment method not enabled
+    '403'          	        =>  'Zabroniono',
+    '400'                   =>  'Wpisałeś złe parametry.',
+    '429'          	        =>  'Wysłałeś już taką płatność. Proszę poczekać do 24 godzin..',
+    'Invoice confirmation'  =>  'Twoja prośba o sprawdzenie zapłaty powiodła się!',
+    'Success'               =>  'Sukcess!', // Used for status title
+    'Success Text'          =>  'Dostaliśmy twoje zamówienie. Badź cierpliwy i poczekaj do 24 godzin aż administrator sprawdzi oraz zatwierdzi twoją płatność',
+    'Failed' 		        =>  'Płatność anulowana.'
+    'Awaiting Approval'     =>  'Masz już oczekującą płatność(ci). Może to potrwać do 24 godzin aż administrator sprawdzi oraz zatwierdzi twoją płatność.'
+);
